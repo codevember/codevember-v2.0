9b41441fa4cfdf454eef00fdcc782c9e0d626e7f
tournevis <arthur.ro92@gmail.com>
2017-10-08 14:36:04 +0200

    📦  set calendar as a component

---
+++ src/components/calendar.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="home">
+    <div class="calendar">
+      <div v-on:mouseover='showPrompt = !showPrompt' v-for="day in 31"class="calendar-card">
+        <div class="calendar-number">
+          <h2 >
+            {{ day < 10 ? 0 + day.toString() : day }}.
+          </h2>
+        </div>
+      </div>
+      <div :class="{fadeIn: showPrompt}" class="prompt-container">
+        <h2 class="prompt">Prompts</h2>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+      showPrompt: false,
+      prompts:[
+        ''
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import "../styles/calendar";
+</style>
