aca6874e9cb3a9edcd205a3d558a4b3348ca4af9
tournevis <arthur.ro92@gmail.com>
2017-10-12 22:33:59 +0200

    💎 add window event and layout builder

--- src/components/calendar.vue
+++ src/components/calendar.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <div class="calendar">
-      <div v-on:mouseover='showPrompt = !showPrompt' v-for="day in 31"class="calendar-card">
+      <div v-on:mouseover='showPrompt = !showPrompt' v-for="day in 30"class="calendar-card">
         <router-link :to="{ name: 'day',params:{ day: day < 10 ? 0 + day.toString() : day }}">
           <div class="calendar-number">
             <h2 >
@@ -24,9 +24,50 @@ export default {
   data () {
     return {
       showPrompt: false,
+      windowResizeRate: 200,
+      timeoutResize :{},
+      layout:[{
+        columns:0,
+        elements:[]
+      }],
       prompts:[
         ''
       ]
+    }
+  },
+  mounted(){
+    this.$nextTick(()=>{
+      this.buildLayout()
+      this.addEvent(window, 'resize', () => {
+        clearTimeout(this.timeoutResize)
+        this.timeoutResize = setTimeout(this.buildLayout(), this.windowResizeRate)
+      })
+    })
+  },
+  methods:{
+    buildLayout(){
+      let tmpLayout = this.$el.querySelectorAll('.calendar-card')
+      let columnsId = 0
+      tmpLayout.length > 0 && this.layout[columnsId].elements.push(tmpLayout[0])
+      for (var i = 1; i < tmpLayout.length; i++) {
+        if(tmpLayout[i].getBoundingClientRect().top != tmpLayout[i-1].getBoundingClientRect().top){
+          columnsId++
+          this.layout.push({
+            columns:columnsId,
+            elements:[]
+          })
+        }
+       this.layout[columnsId].elements.push(tmpLayout[i])
+      }
+    },
+    addEvent (obj, type, fn) {
+      if (obj.addEventListener) {
+        obj.addEventListener(type, fn, false)
+      } else if (obj.attachEvent) {
+        obj.attachEvent('on' + type, () => {
+          return fn.call(obj, window.event)
+        })
+      }
     }
   }
 }
