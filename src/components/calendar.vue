e7be6092bfed8e50165a3e39d11617cd37ba4670
tournevis <arthur.ro92@gmail.com>
2017-10-30 23:27:16 +0100

    🐛  bug fix on calendar animation

--- src/components/calendar.vue
+++ src/components/calendar.vue
@@ -78,7 +78,8 @@ export default {
       let columnsId = 0
       tmpLayout.length > 0 && this.layout[columnsId].elements.push(tmpLayout[0])
       for (var i = 1; i < tmpLayout.length; i++) {
-        if(tmpLayout[i].getBoundingClientRect().top != tmpLayout[i-1].getBoundingClientRect().top){
+        console.log(tmpLayout[i].getBoundingClientRect().top );
+        if((tmpLayout[i].getBoundingClientRect().top != tmpLayout[i-1].getBoundingClientRect().top) || this.layout[columnsId].elements.length >= 7){
           columnsId++
           this.layout.push({
             columns:columnsId,
